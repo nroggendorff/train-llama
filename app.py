6950bd7f3f5ff215f66b9222d6303f7017442c42
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-07-17 02:57:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -59,10 +59,10 @@ print(tokenizer.apply_chat_template([{"role": "user", "content": "Why is the sky
 
 config = LlamaConfig(
     vocab_size=tokenizer.vocab_size,
-    hidden_size=int(512 / 1),
-    intermediate_size=int(1024 / 1),
-    num_hidden_layers=int(8 / 1),
-    num_attention_heads=int(8 / 1),
+    hidden_size=int(512 / 4),
+    intermediate_size=int(1024 / 4),
+    num_hidden_layers=int(8 / 4),
+    num_attention_heads=int(8 / 4),
     max_position_embeddings=int(512 / 1),
     rms_norm_eps=1e-6,
     initializer_range=0.02,
@@ -97,7 +97,7 @@ print(dataset['text'][2])
 
 args = TrainingArguments(
     output_dir="mayo",
-    num_train_epochs=4,
+    num_train_epochs=1,
     gradient_accumulation_steps=4,
     per_device_train_batch_size=1,
     learning_rate=1e-5,

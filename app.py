099e984673e4d5f7cfb76641756f1afbda052888
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-07-19 00:14:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datasets import load_dataset
 from tokenizers import ByteLevelBPETokenizer
 import trl
 
-dataset = load_dataset("nroggendorff/openhermes", split="train").select(range(int(2e+5)))
+dataset = load_dataset("nroggendorff/openhermes", split="train").select(range(int(2e+4)))
 
 def get_training_corpus():
     for i in range(0, len(dataset), 1000):
@@ -98,7 +98,7 @@ print(dataset['text'][2])
 
 args = TrainingArguments(
     output_dir="mayo",
-    num_train_epochs=1,
+    num_train_epochs=8,
     per_device_train_batch_size=64,
     gradient_accumulation_steps=4,
     learning_rate=1e-5,

a9e3e499f095524f891f2200d4101f2952f1d2ae
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-07-24 18:27:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ print(dataset['text'][2])
 args = TrainingArguments(
     output_dir="mayo",
     num_train_epochs=1,
-    per_device_train_batch_size=512,
+    per_device_train_batch_size=256,
     learning_rate=1e-4,
     fp16=True,
     optim="sgd"

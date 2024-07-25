cc8a23dc2ce46a65ce5299f116b87350b1914774
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-07-25 21:47:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from datasets import load_dataset
 from tokenizers import ByteLevelBPETokenizer
 
 MAX_SEQ_LENGTH = 512
-BATCH_SIZE = 1024
+BATCH_SIZE = 768
 EPOCHS = 8
 LEARNING_RATE = 1e-4
 FP16 = True

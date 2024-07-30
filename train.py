8355374a02b33a3a46427990eed4fef1f549ae1b
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-07-30 17:43:23 +0000

    Update train.py

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from tokenizers import ByteLevelBPETokenizer
 
 MAX_SEQ_LENGTH = 512
 BATCH_SIZE = 32
-EPOCHS = 500
+EPOCHS = 1
 LEARNING_RATE = 2e-4
 FACTOR = 8
 VOCAB_SIZE = 32000
@@ -23,7 +23,7 @@ CLIPPING = 1.0
 PUSH_TO_HUB = True
 
 def load_data():
-    dataset = load_dataset(INPUT_DATASET, split="train").select(range(int(2e+4)))
+    dataset = load_dataset(INPUT_DATASET, split="train")#.select(range(int(2e+4)))
     return dataset
 
 def create_tokenizer(training_corpus):

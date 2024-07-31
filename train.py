b8d17c2f55d83fa3fc989eb8044e6962f0ef12e4
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-07-31 20:15:05 +0000

    Update train.py

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from datasets import load_dataset
 from tokenizers import ByteLevelBPETokenizer
 
 MAX_SEQ_LENGTH = 512
-BATCH_SIZE = 512
+BATCH_SIZE = 256
 EPOCHS = 1000
 LEARNING_RATE = 2e-4
 FACTOR = 2

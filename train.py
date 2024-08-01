90c2f9503a8ad217a7d1bb1d5c8a3b9fe7949bcd
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-08-01 15:54:42 +0000

    Update train.py

--- train.py
+++ train.py
@@ -8,10 +8,10 @@ from datasets import load_dataset
 from tokenizers import ByteLevelBPETokenizer
 
 MAX_SEQ_LENGTH = 512
-BATCH_SIZE = 64
-EPOCHS = 3
+BATCH_SIZE = 96
+EPOCHS = 10
 LEARNING_RATE = 2e-2
-FACTOR = 256
+FACTOR = 128
 VOCAB_SIZE = 32000
 INPUT_DATASET = "nroggendorff/openhermes"
 OUTPUT_REPO = "smallama"

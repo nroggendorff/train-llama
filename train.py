ee5aab1129f0d487c83594d7f6d7ca7b735c7bbb
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-08-05 12:19:35 +0000

    Update train.py

--- train.py
+++ train.py
@@ -25,9 +25,9 @@ PUSH_TO_HUB = True
 
 def load_data():
     pretrain = load_dataset(INPUT_DATASET, "cosmopedia-v2", split="train", streaming=True)
-    pretrain = Dataset.from_generator(lambda: pretrain.take(int(3e+4)))
+    pretrain = Dataset.from_generator(lambda: pretrain.take(int(6e+4)))
     instruct = load_dataset(INSTRUCT_DATASET, split="train", streaming=True)
-    instruct = Dataset.from_generator(lambda: instruct.take(int(5e+4)))
+    instruct = Dataset.from_generator(lambda: instruct.take(int(8e+4)))
     dataset_dict = DatasetDict({
         'pretrain': pretrain,
         'instruct': instruct

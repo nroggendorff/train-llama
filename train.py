de3a165bc8392640e4d225fefb08a932a0000a3f
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-08-05 01:24:04 +0000

    Update train.py

--- train.py
+++ train.py
@@ -10,13 +10,13 @@ from tokenizers import ByteLevelBPETokenizer
 MAX_SEQ_LENGTH = 128
 BATCH_SIZE = 64
 EPOCHS = 10
-LEARNING_RATE = 2e-5
+LEARNING_RATE = 1e-4
 FACTOR = 1024
 VOCAB_SIZE = 3200
 INPUT_DATASET = "HuggingFaceTB/smollm-corpus"
 INSTRUCT_DATASET = "nroggendorff/openhermes"
 OUTPUT_REPO = "smallama"
-FP16 = True
+FP16 = False
 WARMUP_STEPS = 0
 DECAY = 0
 GRADIENT_ACCUMULATION_STEPS = 1

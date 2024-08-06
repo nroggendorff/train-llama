51ed2313f86138019067e456b3e6b0cead7cff7c
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-08-06 19:44:07 +0000

    Update train.py

--- train.py
+++ train.py
@@ -10,23 +10,23 @@ from tokenizers import ByteLevelBPETokenizer
 MAX_SEQ_LENGTH = 128
 BATCH_SIZE = 16
 EPOCHS = 2
-LEARNING_RATE = 1e-3
+LEARNING_RATE = 2e-3
 FACTOR = 1024
 VOCAB_SIZE = 32000
 INPUT_DATASET = "HuggingFaceTB/smollm-corpus"
-INSTRUCT_DATASET = "nroggendorff/openhermes"
+INSTRUCT_DATASET = "nroggendorff/elephant"
 OUTPUT_REPO = "smallama"
 FP16 = False
 WARMUP_STEPS = 20
 DECAY = 0
-GRADIENT_ACCUMULATION_STEPS = 16
+GRADIENT_ACCUMULATION_STEPS = 64
 PUSH_TO_HUB = True
 
 def load_data():
     pretrain = load_dataset(INPUT_DATASET, "cosmopedia-v2", split="train", streaming=True)
-    pretrain = Dataset.from_generator(lambda: pretrain.take(int(5e+5)))
+    pretrain = Dataset.from_generator(lambda: pretrain.take(int(5e+6)))
     instruct = load_dataset(INSTRUCT_DATASET, split="train", streaming=True)
-    instruct = Dataset.from_generator(lambda: instruct.take(int(8e+5)))
+    instruct = Dataset.from_generator(lambda: instruct.take(int(3e+6)))
     dataset_dict = DatasetDict({
         'pretrain': pretrain,
         'instruct': instruct

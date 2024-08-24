affb3f974abc36dd1fe16df33fafbfe87a68c0a0
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-08-24 00:35:17 +0000

    Update train.py

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from tokenizers import ByteLevelBPETokenizer
 MAX_SEQ_LENGTH = 512
 BATCH_SIZE = 4
 EPOCHS = 2
-LEARNING_RATE = 2e-5
+LEARNING_RATE = 2e-4
 FACTOR = 1024
 VOCAB_SIZE = 32000
 INPUT_DATASET = "HuggingFaceTB/smollm-corpus"

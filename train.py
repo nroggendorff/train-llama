1f8b4f34d7a5a9dc25deff9e1b77e59a464b6e4f
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-09-04 19:06:31 +0000

    Update train.py

--- train.py
+++ train.py
@@ -16,8 +16,8 @@ VOCAB_SIZE = 32000
 INPUT_DATASET = "HuggingFaceTB/smollm-corpus"
 INSTRUCT_DATASET = "nroggendorff/elephant"
 OUTPUT_REPO = "nroggendorff/smallama"
-INSTRUCT_FINETUNE_BOOL = True
-FP16 = False
+INSTRUCT_FINETUNE_BOOL = False
+FP16 = True
 WARMUP_STEPS = 0
 DECAY = 0
 GRADIENT_ACCUMULATION_STEPS = 1
@@ -26,10 +26,10 @@ PUSH_TO_HUB = True
 def load_data():
     if not INSTRUCT_FINETUNE_BOOL:
         dataset = load_dataset(INPUT_DATASET, "cosmopedia-v2", split="train", streaming=True)
-        dataset = Dataset.from_generator(lambda: dataset.take(int(9e+5)))
+        dataset = Dataset.from_generator(lambda: dataset.take(int(2e+6)))
     else:
         dataset = load_dataset(INSTRUCT_DATASET, split="train", streaming=True)
-        dataset = Dataset.from_generator(lambda: dataset.take(int(5e+5)))
+        dataset = Dataset.from_generator(lambda: dataset.take(int(3e+6)))
     return dataset
 
 def create_tokenizer(training_corpus):

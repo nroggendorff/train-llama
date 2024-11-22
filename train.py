523952fc4ebd2c0699c9af99d5ece3a995517325
Noa Roggendorff <nroggendorff@users.noreply.huggingface.co>
2024-11-22 09:09:21 +0000

    Update train.py

--- train.py
+++ train.py
@@ -53,7 +53,7 @@ def main(push_to_hub=True):
 
     print("Training Model..")
     train_model(model, tokenizer, dataset, push_to_hub)
-    raise FineError("Trained Model.")
+    raise Conclusion("Trained Model.")
 
 if __name__ == "__main__":
     try:

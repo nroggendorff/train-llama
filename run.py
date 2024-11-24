a1b758dc12b2a5e960999c533f27539d439c7cd8
nroggendorff <nroggendorff87@gmail.com>
2024-11-24 15:22:14 -0600

    f

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 from huggingface_hub import HfApi
 
-HfApi().upload_folder(
+api = HfApi()
+repo_id = "nroggendorff/train-llama"
+
+pr_id = api.upload_folder(
     folder_path="./Run",
-    repo_id="nroggendorff/train-llama",
+    repo_id=repo_id,
     repo_type="space",
     create_pr=True,
-    commit_message="Merge when ready" #grr
-)
+    commit_message="Merge when ready"
+).pr_revision
+
+api.change_discussion_status(repo_id, pr_id, 'closed', repo_type='space')

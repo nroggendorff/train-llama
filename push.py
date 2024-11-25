7d427e151a16fdba1b88aae4eacb444f72df48fe
nroggendorff <nroggendorff87@gmail.com>
2024-11-24 19:29:06 -0600

    shrug

--- push.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import HfApi
-
-HfApi().upload_folder(
-    folder_path="./Push",
-    repo_id="nroggendorff/train-llama",
-    repo_type="space",
-)
--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 from huggingface_hub import HfApi
+from random import randint
 
 api = HfApi()
 repo_id = "nroggendorff/train-llama"
+
+comment = "Merge when ready " + ''.join([chr(u) for u in [randint(33, 126) for _ in range(8)]])
 
 pr_id = api.upload_folder(
     folder_path="./Run",
     repo_id=repo_id,
     repo_type="space",
     create_pr=True,
-    commit_message="Merge when ready"
-).pr_revision
+    commit_message=comment
+).pr_num - 1
+
+isnt_v = api.get_discussion_details(repo_id, pr_id, repo_type='space').title == comment
 
 api.change_discussion_status(repo_id, pr_id, 'closed', repo_type='space')

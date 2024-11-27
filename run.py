60892bfb9f2f8a36e0a75ae49be7865721aaca72
nroggendorff <nroggendorff87@gmail.com>
2024-11-27 14:39:00 -0600

    update

--- run.py
+++ run.py
@@ -11,15 +11,19 @@ pr_id = api.upload_folder(
     repo_id=repo_id,
     repo_type="space",
     create_pr=True,
+    force=True,
     commit_message=comment
 ).pr_num
 
-lastpr = pr_id - 1
+try:
+    lastpr = pr_id - 1
 
-for _ in range(5):
-    item_deets = api.get_discussion_details(repo_id, lastpr, repo_type='space')
-    if item_deets.title == comment and item_deets.status == "open":
-        api.change_discussion_status(repo_id, lastpr, 'closed', repo_type='space')
-        break
-    else:
-        lastpr -= 1
+    for _ in range(5):
+        item_deets = api.get_discussion_details(repo_id, lastpr, repo_type='space')
+        if item_deets.title == comment and item_deets.status == "open":
+            api.change_discussion_status(repo_id, lastpr, 'closed', repo_type='space')
+            break
+        else:
+            lastpr -= 1
+except TypeError:
+    print("No diff detected.")

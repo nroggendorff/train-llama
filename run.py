589121fcfa82486609eabb905871cc79b5a33e27
nroggendorff <nroggendorff87@gmail.com>
2024-12-08 15:20:42 -0600

    more or less complicated

--- run.py
+++ run.py
@@ -1,18 +1,30 @@
 from huggingface_hub import HfApi
 from random import randint
+import tempfile
 
 api = HfApi()
 repo_id = "nroggendorff/train-llama"
 
+file_content = "FROM nroggendorff/train-llama:latest \n" \
+"RUN jq '.init = 0 | .\"instruct-finetune-bool\" = false' config.json > temp.json && \\ \n" \
+"    mv temp.json config.json && \\ \n" \
+"    chown -R user:user config.json \n" \
+"CMD [\"bash\", \"-c\", \"python prep.py && deepspeed --num_gpus=$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader | wc -l) train.py\"]"
+print(file_content)
 comment = "Merge when ready " + ''.join([chr(u) for u in [randint(33, 126) for _ in range(8)]])
 
-pr_id = api.upload_folder(
-    folder_path="./Run",
-    repo_id=repo_id,
-    repo_type="space",
-    create_pr=True,
-    commit_message=comment
-).pr_num
+with tempfile.NamedTemporaryFile(mode='w', suffix='.dockerfile') as tmp_file:
+    tmp_file.write(file_content)
+    tmp_file.flush()
+    
+    pr_id = api.upload_file(
+        path_or_fileobj=tmp_file.name,
+        path_in_repo="Dockerfile",
+        repo_id=repo_id,
+        repo_type="space",
+        create_pr=True,
+        commit_message=comment
+    ).pr_num
 
 try:
     lastpr = pr_id - 1

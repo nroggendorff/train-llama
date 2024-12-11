c6d306ad71f88718e0ee4871b8bac09a5c24f627
nroggendorff <nroggendorff87@gmail.com>
2024-12-11 14:48:49 -0600

    test

--- Build/trainer.py
+++ Build/trainer.py
@@ -1,15 +1,19 @@
-import train
 import prep
 
+import torch
+
 import os
+import subprocess
 
 from util import Space
 from doubt import discord_logging
 
+devices = torch.cuda.device_count()
+
 @discord_logging(app_name='trainer', webhook_url=os.getenv('DISCORD_WEBHOOK_URL'))
 def trainer():
     prep.main()
-    train.main()
+    subprocess.run(["deepspeed", f"--num_gpus={devices}", "train.py"])
 
 if __name__ == "__main__":
     try:

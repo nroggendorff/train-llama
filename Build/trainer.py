2279e732da1386016c2055fe9dbc0090995a7d68
nroggendorff <nroggendorff87@gmail.com>
2024-12-11 14:28:34 -0600

    hmm

--- Build/trainer.py
+++ Build/trainer.py
@@ -8,12 +8,12 @@ from doubt import discord_logging
 
 @discord_logging(app_name='trainer', webhook_url=os.getenv('DISCORD_WEBHOOK_URL'))
 def trainer():
+    prep.main()
+    train.main()
+
+if __name__ == "__main__":
     try:
-        prep.main()
-        train.main()
+        trainer()
     except Exception as e:
         print(f'{type(e).__name__}: {e}')
         Space().stop()
-
-if __name__ == "__main__":
-    trainer()

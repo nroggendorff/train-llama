6688b726fee397a8c36ed455b2ebce49a619baf4
nroggendorff <nroggendorff87@gmail.com>
2024-12-16 16:38:39 -0600

    fix potential pickleerror

--- util.py
+++ util.py
@@ -9,9 +9,15 @@ class Conclusion(Exception):
 class Space:
     def __init__(self):
         self.api = HfApi()
-        self.pause = lambda: self.api.pause_space(f"{os.getenv('SPACE_AUTHOR_NAME')}/{os.getenv('SPACE_REPO_NAME')}")
+        self.repo_id = f"{os.getenv('SPACE_AUTHOR_NAME')}/{os.getenv('SPACE_REPO_NAME')}"
 
     def stop(self, message=None):
         if message:
             raise Conclusion(f"{type(message).__name__}: {message}")
         raise Conclusion()
+
+    def pause(self):
+        self.api.pause_space(self.repo_id)
+
+    def resume(self):
+        self.api.restart_space(self.repo_id)

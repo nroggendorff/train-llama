f198c43bcaa9c3a03f5033aaf249b59a17f8bbe7
nroggendorff <nroggendorff87@gmail.com>
2025-09-18 07:56:38 -0500

    new ending logic to remove a lot of manual work

--- util.py
+++ util.py
@@ -1,6 +1,20 @@
 import os
 from huggingface_hub import HfApi
 
+from config import Config
+
+import json
+import os
+
+def get_dataset_size():
+    with open(os.path.join("prepared_dataset", "dataset_info.json"), "r") as f:
+        info = json.load(f)
+    total = 0
+    for split in info.get("splits", {}).values():
+        total += split.get("num_examples", 0)
+    return total
+
+config = Config()
 
 class Conclusion(Exception):
     def __init__(self, message="Script execution has completed."):
@@ -24,3 +38,27 @@ class Space:
 
     def resume(self):
         self.api.restart_space(self.repo_id)
+
+    def reset(self, init=config.INIT, inst=config.INSTRUCT_FINETUNE_BOOL, shard_size=config.SHARD_SIZE):
+        dataset_size = get_dataset_size()
+
+        if dataset_size > shard_size * (init + 2):
+            if not inst:
+                new_init = 0
+                new_instruct = True
+            else:
+                return self.pause()
+        else:
+            new_init = init + 1
+            new_instruct = inst
+
+        self.api.add_space_variable(
+            repo_id=self.repo_id,
+            key="INIT",
+            value=str(new_init),
+        )
+        self.api.add_space_variable(
+            repo_id=self.repo_id,
+            key="INSTRUCT",
+            value="true" if new_instruct else "false",
+        )

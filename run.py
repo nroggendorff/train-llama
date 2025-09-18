3f7e592ccc1acd70fb9652c117dc6ff20831e22f
nroggendorff <nroggendorff87@gmail.com>
2025-09-18 11:56:11 -0500

    remove deprecated things

--- run.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import HfApi
-
-api = HfApi()
-repo_id = "nroggendorff/train-llama"
-
-try:
-    api.restart_space(repo_id, factory_reboot=True)
-except Exception as e:
-    print(f"Error restarting space: {e}")

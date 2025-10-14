7988bb77b6e08fd1239fd774c8520cf437d24612
nroggendorff <nroggendorff87@gmail.com>
2025-10-13 22:12:01 -0500

    not complicated enough

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 import re
+import math
 from trl import SFTConfig
 
 
@@ -17,12 +18,12 @@ class Config:
             return
 
         epochs = float(os.environ.get("EPOCHS", 3))
+        lr = float(os.environ.get("LEARNING_RATE", 3e-4))
         space_timeout = os.environ.get("STARTUP_DURATION_TIMEOUT", "350m")
         int_space_timeout = int(re.sub(r"\D", "", space_timeout))
         self.BATCH_SIZE = int(os.environ.get("BATCH_SIZE", 4))
         self.INIT = int(os.environ.get("INIT", 0))
         self.EPOCHS = epochs if self.INIT >= 2 else epochs / 2
-        self.LEARNING_RATE = float(os.environ.get("LEARNING_RATE", 3e-4))
         self.MAX_LENGTH = int(os.environ.get("MAX_LENGTH", 2048))
         self.VOCAB_SIZE = int(os.environ.get("VOCAB_SIZE", 52000))
         self.FP16 = True
@@ -35,6 +36,16 @@ class Config:
         self.SHARD_SIZE = int(os.environ.get("SHARD_SIZE", 131072))
         self.SHARD_INDEX = 0 if self.INIT < 2 else (self.INIT - 1)
         self.SKIP_SAMPLES = self.SHARD_INDEX * self.SHARD_SIZE
+        self.LEARNING_RATE = (
+            lr
+            / math.sqrt(self.INIT + 1)
+            ** (
+                ((self.SHARD_SIZE / self.EPOCHS) * (self.INIT + self.EPOCHS))
+                / (self.SHARD_SIZE + self.EPOCHS)
+            )
+            if self.INIT > 1
+            else lr
+        )
         self.OUTPUT_REPO = os.environ.get("OUTPUT_REPO", "nroggendorff/smallama")
         self.INPUT_REPO = os.environ.get("INPUT_REPO", self.OUTPUT_REPO)
         self.INSTRUCT_FINETUNE_BOOL = os.environ.get("INST", "false").lower() == "true"

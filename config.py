139ceaf53207bf3812cf1ddf660117e8226aa3af
nroggendorff <nroggendorff87@gmail.com>
2025-11-14 11:03:13 -0600

    reformat config

--- config.py
+++ config.py
@@ -16,29 +16,42 @@ class Config:
         if Config._initialized:
             return
 
-        epochs = float(os.environ.get("EPOCHS", "3"))
-        lr = float(os.environ.get("LEARNING_RATE", "3e-4"))
+        self._load_environment_variables()
+        self._calculate_training_parameters()
+        self._setup_repositories()
+        self._calculate_timing()
+
+        Config._initialized = True
+
+    def _load_environment_variables(self):
+        self.epochs = float(os.environ.get("EPOCHS", "3"))
+        self.lr = float(os.environ.get("LEARNING_RATE", "3e-4"))
+
         self.FACTOR = int(os.environ.get("FACTOR", "12288"))
         self.VOCAB_SIZE = int(os.environ.get("VOCAB_SIZE", "52000"))
-
-        base_output_repo = os.environ.get("OUTPUT_REPO", "nroggendorff/smallama")
-        model_size_suffix = self._calculate_model_size_suffix()
-
-        space_timeout = os.environ.get("STARTUP_DURATION_TIMEOUT", "350m")
-        int_space_timeout = int(re.sub(r"\D", "", space_timeout))
         self.BATCH_SIZE = int(os.environ.get("BATCH_SIZE", "4"))
         self.INIT = int(os.environ.get("INIT", "0"))
-        self.INSTRUCT_FINETUNE_BOOL = os.environ.get("INST", "false").lower() == "true"
         self.MAX_LENGTH = int(os.environ.get("MAX_LENGTH", "2048"))
-        self.FP16 = True
         self.WEIGHT_DECAY = float(os.environ.get("WEIGHT_DECAY", "1e-2"))
         self.GRADIENT_ACCUMULATION_STEPS = int(
             os.environ.get("GRADIENT_ACCUMULATION_STEPS", "2")
         )
+        self.SHARD_SIZE = int(os.environ.get("SHARD_SIZE", "131072"))
+        self.TIMEOUT_BUFFER = int(os.environ.get("TIMEOUT_BUFFER", "20"))
+
+        self.FP16 = True
+        self.MAX_RETRIES = 5
+        self.SEED = 42
+
+        self.INSTRUCT_FINETUNE_BOOL = os.environ.get("INST", "false").lower() == "true"
+        self.INST_SUFFIX = os.environ.get("INST_SUFFIX", "it")
+
         self.INPUT_DATASET = os.environ.get("INPUT_DS", "nroggendorff/microrpus")
         self.INSTRUCT_DATASET = os.environ.get("INST_DS", "nroggendorff/elephant")
-        self.SHARD_SIZE = int(os.environ.get("SHARD_SIZE", "131072"))
-        self.INST_SUFFIX = os.environ.get("INST_SUFFIX", "it")
+        self.base_output_repo = os.environ.get("OUTPUT_REPO", "nroggendorff/smallama")
+
+        self.space_timeout = os.environ.get("STARTUP_DURATION_TIMEOUT", "350m")
+
         self.CUSTOM_PROCESSOR = os.environ.get(
             "CUSTOM_PROCESSOR",
             (
@@ -54,58 +67,59 @@ class Config:
             ),
         )
 
+    def _calculate_training_parameters(self):
         if self.INSTRUCT_FINETUNE_BOOL:
             self.SHARD_INDEX = self.INIT
             self.SKIP_SAMPLES = self.INIT * self.SHARD_SIZE
-            self.EPOCHS = epochs
+            self.EPOCHS = self.epochs
         elif self.INIT == 0:
             self.SHARD_INDEX = 0
             self.SKIP_SAMPLES = 0
-            self.EPOCHS = epochs / 2
+            self.EPOCHS = self.epochs / 2
             self.SHARD_SIZE = self.SHARD_SIZE // 2
         elif self.INIT == 1:
             self.SHARD_INDEX = 0
             self.SKIP_SAMPLES = self.SHARD_SIZE // 2
-            self.EPOCHS = epochs / 2
+            self.EPOCHS = self.epochs / 2
             self.SHARD_SIZE = self.SHARD_SIZE // 2
         else:
             self.SHARD_INDEX = self.INIT - 1
             self.SKIP_SAMPLES = self.SHARD_SIZE + (self.INIT - 2) * self.SHARD_SIZE
-            self.EPOCHS = epochs
+            self.EPOCHS = self.epochs
+
         if self.INIT > 1:
             decay_factor = 1.0 / (1.0 + 0.1 * (self.INIT - 1))
-            self.LEARNING_RATE = max(lr * decay_factor, lr * 0.1)
+            self.LEARNING_RATE = max(self.lr * decay_factor, self.lr * 0.1)
         else:
-            self.LEARNING_RATE = lr
-        self.OUTPUT_REPO = f"{base_output_repo}-{model_size_suffix}"
-        self.INPUT_REPO = os.environ.get("INPUT_REPO", self.OUTPUT_REPO)
-        self.INPUT_TOKENIZER = os.environ.get("INPUT_TOK", self.INPUT_REPO)
+            self.LEARNING_RATE = self.lr
+
         self.TOTAL_STEPS = (self.SHARD_SIZE * self.EPOCHS) // (
             self.BATCH_SIZE * self.GRADIENT_ACCUMULATION_STEPS
         )
         self.WARMUP_STEPS = 0 if self.INIT > 0 else int(self.TOTAL_STEPS * 0.1)
-        self.SPACE_TIMEOUT = (
-            int_space_timeout
-            if space_timeout.endswith("m")
-            else (
-                int_space_timeout * 60
-                if space_timeout.endswith("h")
-                else (
-                    print(
-                        "Invalid STARTUP_DURATION_TIMEOUT format.",
-                        "Use 'm' for minutes or 'h' for hours.",
-                        "Falling back to 30 minutes.",
-                    )
-                    or 30
-                )
-            )
-        )
-        self.TIMEOUT_BUFFER = int(os.environ.get("TIMEOUT_BUFFER", "20"))
-        self.TIMEOUT = self.SPACE_TIMEOUT - self.TIMEOUT_BUFFER
-        self.MAX_RETRIES = 5
-        self.SEED = 42
 
-        Config._initialized = True
+    def _setup_repositories(self):
+        model_size_suffix = self._calculate_model_size_suffix()
+        self.OUTPUT_REPO = f"{self.base_output_repo}-{model_size_suffix}"
+        self.INPUT_REPO = os.environ.get("INPUT_REPO", self.OUTPUT_REPO)
+        self.INPUT_TOKENIZER = os.environ.get("INPUT_TOK", self.INPUT_REPO)
+
+    def _calculate_timing(self):
+        int_space_timeout = int(re.sub(r"\D", "", self.space_timeout))
+
+        if self.space_timeout.endswith("m"):
+            self.SPACE_TIMEOUT = int_space_timeout
+        elif self.space_timeout.endswith("h"):
+            self.SPACE_TIMEOUT = int_space_timeout * 60
+        else:
+            print(
+                "Invalid STARTUP_DURATION_TIMEOUT format.",
+                "Use 'm' for minutes or 'h' for hours.",
+                "Falling back to 30 minutes.",
+            )
+            self.SPACE_TIMEOUT = 30
+
+        self.TIMEOUT = self.SPACE_TIMEOUT - self.TIMEOUT_BUFFER
 
     def _calculate_model_size_suffix(self):
         hidden_size = self.FACTOR
